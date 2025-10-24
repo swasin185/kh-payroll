657152552c669c423324e859a301e041df114666
TomNJerry <swasin185@gmail.com>
2025-10-24 22:34:41 +0700

    hook page:extend keepalive

--- app/plugins/0 init.ts
+++ app/plugins/0 init.ts
@@ -1,3 +1,8 @@
 export default defineNuxtPlugin(async (nuxtApp) => {
     console.log("App initialized at startup")
+
+    // make every pages keep alive while navigate push/back
+    nuxtApp.hook('page:extend', (page) => {
+        page.meta.keepalive ??= true
+    })
 })
