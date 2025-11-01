ee44ededf0e22da30aa2dd0fd5066f5c1899c056
Tommy <swasin185@gmail.com>
2025-11-01 15:08:21 +0700

    docker no https

--- script/docker/run.sh
+++ script/docker/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker build -t kh-payroll:latest -f script/docker/Dockerfile .
-docker-compose up -d
+docker image prune
+docker run -d \
+  --name kh-payroll-server \
+  --network "host" \
+  kh-payroll:latest
