289288d1179176c7b9e34a6a055a015e28a328ae
Tommy <swasin185@gmail.com>
2025-11-02 18:49:21 +0700

    docker log

--- server/api/auth/local.post.ts
+++ server/api/auth/local.post.ts
@@ -6,19 +6,17 @@ export default eventHandler(async (event) => {
     const body = await readBody(event)
     const userid = body.id?.toString().toLowerCase()
     const password = body.pwd?.toString()
-    console.log("LOGIN:", userid)
     if (!userid || !password) {
         throw createError({
             status: 400,
             message: "User ID and Password are required.",
         })
     }
+    console.log("LOGIN:", userid)
     try {
         const authUser = await SqlUsers.select(userid)
-        console.log(1)
         if (authUser && (authUser.passwd == null || authUser.passwd === password)) {
             const company = await SqlCompany.select(authUser.comCode!)
-            console.log(2)
             await setUserSession(event, {
                 user: {
                     id: authUser.id,
@@ -30,11 +28,8 @@ export default eventHandler(async (event) => {
                     mnPayroll: company?.mnPayroll,
                 },
             })
-            console.log(3)
             const sess = await getUserSession(event)
-            console.log(4)
             await renewSession(sess?.id, authUser.id)
-            console.log(5)
             return true
         } else {
             return false
