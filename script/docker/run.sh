0e91699128dc46fe7fde22992536018c96ea52cb
Tommy <swasin185@gmail.com>
2025-11-02 09:59:35 +0700

    docker context

--- script/docker/run.sh
+++ script/docker/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker build -t kh-payroll:latest .
+# docker build -t kh-payroll:latest -f script/docker/Dockerfile ../..
+docker-compose pull
+docker-compose build
 docker image prune -f
 docker-compose up -d
