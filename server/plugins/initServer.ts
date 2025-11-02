e6ffdd854ffc9c2e4b83ed7336e90b35b3e49d87
Tommy <swasin185@gmail.com>
2025-11-02 20:19:22 +0700

    log users start

--- server/plugins/initServer.ts
+++ server/plugins/initServer.ts
@@ -1,8 +1,14 @@
 import { getDB } from "~~/server/database/pool"
-
-export default defineNitroPlugin((nitroApp) => {
+import SqlUsers from "../database/SqlUsers"
+import SqlCompany from "../database/SqlCompany"
+export default defineNitroPlugin(async (nitroApp) => {
     console.log("Server initialized at startup")
-
+    try {
+        const users = await SqlUsers.lookup()
+        console.log(users)
+    } catch (error) {
+        console.log(error)
+    }
     nitroApp.hooks.hookOnce("close", async () => {
         console.info("[DB] Closing MariaDB pool...")
         await getDB().end()
