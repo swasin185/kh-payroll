900f72d2d666171387b58d290c95f066d4068828
swasin185 <swasin185@gmail.com>
2026-02-24 10:38:58 +0700

    clear test case

--- script/sql/04-views.sql
+++ script/sql/04-views.sql
@@ -1,176 +1,187 @@
-CREATE OR REPLACE VIEW `vTimeCard` AS
-select `v`.`scanCode` AS `scanCode`,
-	date_format(
-		if(
-			cast(`v`.`scanAt` as time) < '06:00',
-			`v`.`scanAt` - interval 1 day,
-			`v`.`scanAt`
-		),
-		'%Y-%m-%d'
-	) COLLATE utf8mb4_general_ci AS `dateAt`,
-	date_format(`v`.`scanAt`, '%H:%i') COLLATE utf8mb4_general_ci AS `timeAt`
-from `timecard` v;
+create or replace view `vTimeCard` as
+select
+    `v`.`scanCode` as `scanCode`,
+    date_format(
+        if (
+            cast(`v`.`scanAt` as time) < '06:00',
+            `v`.`scanAt` - interval 1 day,
+            `v`.`scanAt`
+        ),
+        '%Y-%m-%d'
+    ) collate utf8mb4_general_ci as `dateAt`,
+    date_format(`v`.`scanAt`, '%H:%i') collate utf8mb4_general_ci as `timeAt`
+from
+    `timecard` v;
+
 --
-CREATE OR REPLACE VIEW `vDailyTime` AS
-select `v`.`dateAt` AS `dateAt`,
-	`v`.`scanCode` AS `scanCode`,
-	max(if(`v`.`timeAt` < '06:00', `v`.`timeAt`, NULL)) AS `early`,
-	max(
-		if(
-			`v`.`timeAt` >= '06:00'
-			and `v`.`timeAt` <= '10:00',
-			`v`.`timeAt`,
-			NULL
-		)
-	) AS `morning`,
-	min(
-		if(
-			`v`.`timeAt` >= '11:00'
-			and `v`.`timeAt` <= '13:30',
-			`v`.`timeAt`,
-			NULL
-		)
-	) AS `lunch_out`,
-	max(
-		if(
-			`v`.`timeAt` >= '11:30'
-			and `v`.`timeAt` <= '15:30',
-			`v`.`timeAt`,
-			NULL
-		)
-	) AS `lunch_in`,
-	max(
-		if(
-			`v`.`timeAt` >= '15:31'
-			and `v`.`timeAt` <= '18:00',
-			`v`.`timeAt`,
-			NULL
-		)
-	) AS `evening`,
-	max(if(`v`.`timeAt` >= '18:01', `v`.`timeAt`, NULL)) AS `night`,
-	count(0) AS `count`,
-	group_concat(`v`.`timeAt` separator ',') AS `rawTime`
-from `vTimeCard` v
-group by `v`.`dateAt`,
-	`v`.`scanCode`;
+create or replace view `vDailyTime` as
+select
+    `v`.`dateAt` as `dateAt`,
+    `v`.`scanCode` as `scanCode`,
+    max(if (`v`.`timeAt` < '06:00', `v`.`timeAt`, null)) as `early`,
+    max(
+        if (
+            `v`.`timeAt` >= '06:00'
+            and `v`.`timeAt` <= '10:00',
+            `v`.`timeAt`,
+            null
+        )
+    ) as `morning`,
+    min(
+        if (
+            `v`.`timeAt` >= '11:00'
+            and `v`.`timeAt` <= '13:30',
+            `v`.`timeAt`,
+            null
+        )
+    ) as `lunch_out`,
+    max(
+        if (
+            `v`.`timeAt` >= '11:30'
+            and `v`.`timeAt` <= '15:30',
+            `v`.`timeAt`,
+            null
+        )
+    ) as `lunch_in`,
+    max(
+        if (
+            `v`.`timeAt` >= '15:31'
+            and `v`.`timeAt` <= '18:00',
+            `v`.`timeAt`,
+            null
+        )
+    ) as `evening`,
+    max(if (`v`.`timeAt` >= '18:01', `v`.`timeAt`, null)) as `night`,
+    count(0) as `count`,
+    group_concat(`v`.`timeAt` separator ',') as `rawTime`
+from
+    `vTimeCard` v
+group by
+    `v`.`dateAt`,
+    `v`.`scanCode`;
+
 --
-CREATE OR REPLACE VIEW `vAttendance` AS
-select `v2`.*,
-	CASE
-		WHEN v2.status = 'FullDay' THEN v2.fullWorkMin - v2.lateMin1 - v2.lateMin2
-		WHEN v2.status = 'HalfDay' THEN v2.halfWorkMin - v2.lateMin1 - v2.lateMin2
-		ELSE 0
-	END AS workMin
-from (
-		select 
-		    `v`.*,
-			GREATEST(
-				0,
-				COALESCE(v.morning_m, v.fullWorkMin) - v.fullWorkMin
-			) AS lateMin1,
-			IF(
-				v.lunch_out_m IS NULL
-				OR v.lunch_in_m IS NULL
-				OR v.lunch_out_m = v.lunch_in_m,
-				0,
-				v.lunch_in_m - v.lunch_out_m
-			) AS lunchMin,
-			CASE
-				WHEN v.status = 'FullDay' THEN IF(
-					v.lunch_out_m IS NULL
-					OR v.lunch_in_m IS NULL
-					OR v.lunch_out_m = v.lunch_in_m,
-					IF(
-						v.lunch_out_m IS NULL
-						AND v.lunch_in_m IS NULL,
-						v.halfWorkMin,
-						v.halfWorkMin / 2
-					),
-					GREATEST(0, v.lunch_in_m - v.lunch_out_m - 60)
-				) + IF(
-					v.night_m IS NULL
-					AND v.early_m IS NULL
-					AND v.evening_m IS NOT NULL,
-					GREATEST(0, 1020 - v.evening_m),
-					0
-				)
-				WHEN v.status = 'HalfDay' THEN GREATEST(
-					0,
-					v.halfWorkMin - GREATEST(
-						0,
-						COALESCE(v.morning_m, v.fullWorkMin) - v.fullWorkMin
-					) - CASE
-						WHEN v.morning_m IS NOT NULL
-						AND v.lunch_out_m IS NOT NULL THEN LEAST(
-							v.halfWorkMin,
-							v.lunch_out_m - GREATEST(v.morning_m, v.fullWorkMin)
-						)
-						WHEN v.lunch_in_m IS NOT NULL
-						AND COALESCE(v.early_m, v.night_m, v.evening_m) IS NOT NULL THEN LEAST(
-							v.halfWorkMin,
-							COALESCE(v.early_m, v.night_m, v.evening_m) - GREATEST(v.lunch_in_m, 780)
-						)
-						ELSE 0
-					END
-				)
-				ELSE 0
-			END AS lateMin2,
-			IF(
-				v.morning_m IS NULL
-				OR (
-					v.status = 'FullDay'
-					AND v.night_m IS NULL
-					AND v.early_m IS NULL
-				),
-				0,
-				CASE
-					WHEN v.early_m IS NOT NULL THEN v.early_m - 1080
-					WHEN v.night_m IS NOT NULL THEN v.night_m - 1080
-					ELSE 0
-				END
-			) AS otMin
-		from (
-				select 
-				    240 AS halfWorkMin,
-					480 AS fullWorkMin,
-					FLOOR(TIME_TO_SEC(morning) / 60) AS morning_m,
-					FLOOR(TIME_TO_SEC(lunch_out) / 60) AS lunch_out_m,
-					FLOOR(TIME_TO_SEC(lunch_in) / 60) AS lunch_in_m,
-					FLOOR(TIME_TO_SEC(evening) / 60) AS evening_m,
-					FLOOR(TIME_TO_SEC(night) / 60) AS night_m,
-					FLOOR(TIME_TO_SEC(early) / 60) + 1440 AS early_m,
-					comCode,
-					empCode,
-					dateAt,
-					morning,
-					lunch_out,
-					lunch_in,
-					evening,
-					night,
-					early,
-					case
-						when (
-							`a`.`morning` is not null
-							and (
-								`a`.`evening` is not null
-								or `a`.`night` is not null
-								or `a`.`early` is not null
-							)
-						) then 'FullDay'
-						when (
-							(`a`.`morning` is not null)
-							xor (
-								`a`.`evening` is not null
-								or `a`.`night` is not null
-								or `a`.`early` is not null
-							)
-						)
-						and (
-							`a`.`lunch_out` is not null
-							or `a`.`lunch_in` is not null
-						) then 'HalfDay'
-						else 'Absent'
-					end AS `status`
-				from `attendance` `a`
-			) v
-	) v2;
+create or replace view `vAttendance` as
+select
+    `v2`.*,
+    case
+        when v2.status = 'FullDay' then v2.fullWorkMin - v2.lateMin1 - v2.lateMin2
+        when v2.status = 'HalfDay' then v2.halfWorkMin - v2.lateMin1 - v2.lateMin2
+        else 0
+    end as workMin
+from
+    (
+        select
+            `v`.*,
+            greatest(
+                0,
+                coalesce(v.morning_m, v.fullWorkMin) - v.fullWorkMin
+            ) as lateMin1,
+            if (
+                v.lunch_out_m is null
+                or v.lunch_in_m is null
+                or v.lunch_out_m = v.lunch_in_m,
+                0,
+                v.lunch_in_m - v.lunch_out_m
+            ) as lunchMin,
+            case
+                when v.status = 'FullDay' then if (
+                    v.lunch_out_m is null
+                    or v.lunch_in_m is null
+                    or v.lunch_out_m = v.lunch_in_m,
+                    if (
+                        v.lunch_out_m is null
+                        and v.lunch_in_m is null,
+                        v.halfWorkMin,
+                        v.halfWorkMin / 2
+                    ),
+                    greatest(0, v.lunch_in_m - v.lunch_out_m - 60)
+                ) + if (
+                    v.night_m is null
+                    and v.early_m is null
+                    and v.evening_m is not null,
+                    greatest(0, 1020 - v.evening_m),
+                    0
+                )
+                when v.status = 'HalfDay' then greatest(
+                    0,
+                    v.halfWorkMin - greatest(
+                        0,
+                        coalesce(v.morning_m, v.fullWorkMin) - v.fullWorkMin
+                    ) - case
+                        when v.morning_m is not null
+                        and v.lunch_out_m is not null then least(
+                            v.halfWorkMin,
+                            v.lunch_out_m - greatest(v.morning_m, v.fullWorkMin)
+                        )
+                        when v.lunch_in_m is not null
+                        and coalesce(v.early_m, v.night_m, v.evening_m) is not null then least(
+                            v.halfWorkMin,
+                            coalesce(v.early_m, v.night_m, v.evening_m) - greatest(v.lunch_in_m, 780)
+                        )
+                        else 0
+                    end
+                )
+                else 0
+            end as lateMin2,
+            if (
+                v.morning_m is null
+                or (
+                    v.status = 'FullDay'
+                    and v.night_m is null
+                    and v.early_m is null
+                ),
+                0,
+                case
+                    when v.early_m is not null then v.early_m - 1080
+                    when v.night_m is not null then v.night_m - 1080
+                    else 0
+                end
+            ) as otMin
+        from
+            (
+                select
+                    240 as halfWorkMin,
+                    480 as fullWorkMin,
+                    floor(time_to_sec(morning) / 60) as morning_m,
+                    floor(time_to_sec(lunch_out) / 60) as lunch_out_m,
+                    floor(time_to_sec(lunch_in) / 60) as lunch_in_m,
+                    floor(time_to_sec(evening) / 60) as evening_m,
+                    floor(time_to_sec(night) / 60) as night_m,
+                    floor(time_to_sec(early) / 60) + 1440 as early_m,
+                    comCode,
+                    empCode,
+                    dateAt,
+                    morning,
+                    lunch_out,
+                    lunch_in,
+                    evening,
+                    night,
+                    early,
+                    case
+                        when (
+                            `a`.`morning` is not null
+                            and (
+                                `a`.`evening` is not null
+                                or `a`.`night` is not null
+                                or `a`.`early` is not null
+                            )
+                        ) then 'FullDay'
+                        when (
+                            (`a`.`morning` is not null)
+                            xor (
+                                `a`.`evening` is not null
+                                or `a`.`night` is not null
+                                or `a`.`early` is not null
+                            )
+                        )
+                        and (
+                            `a`.`lunch_out` is not null
+                            or `a`.`lunch_in` is not null
+                        ) then 'HalfDay'
+                        else 'Absent'
+                    end as `status`
+                from
+                    `attendance` `a`
+            ) v
+    ) v2;
--- script/sql/05-procedure.sql
+++ script/sql/05-procedure.sql
@@ -1,59 +1,65 @@
 DROP PROCEDURE IF EXISTS payroll.runTimeCard;
 
-DELIMITER $$
-$$
-CREATE PROCEDURE payroll.runTimeCard(in pDateFrom varchar(10), in pDateTo varchar(10))
+
+DELIMITER $$ $$
+CREATE PROCEDURE payroll.runTimeCard (in pDateFrom varchar(10), in pDateTo varchar(10))
 BEGIN
-	
-INSERT INTO attendance (
-    comCode,
-    empCode,
-    dateAt,
-    morning,
-    evening,
-    night,
-    early,
-    lunch_out,
-    lunch_in,
-    scanCount
-  )
-SELECT e.comCode,
-  e.empCode,
-  t.dateAt,
-  t.morning,
-  t.evening,
-  t.night,
-  t.early,
-  t.lunch_out,
-  t.lunch_in,
-  t.count
-FROM vDailyTime t
-  JOIN employee e ON t.scanCode = e.scanCode
-WHERE t.dateAt BETWEEN pDateFrom AND pDateTo;
+INSERT INTO
+    attendance (
+        comCode,
+        empCode,
+        dateAt,
+        morning,
+        evening,
+        night,
+        early,
+        lunch_out,
+        lunch_in,
+        scanCount
+    )
+SELECT
+    e.comCode,
+    e.empCode,
+    t.dateAt,
+    t.morning,
+    t.evening,
+    t.night,
+    t.early,
+    t.lunch_out,
+    t.lunch_in,
+    t.count
+FROM
+    vDailyTime t
+    JOIN employee e ON t.scanCode = e.scanCode
+WHERE
+    t.dateAt BETWEEN pDateFrom AND pDateTo;
 
-END$$
-DELIMITER ;
 
---
+END $$ DELIMITER;
+
 
 DROP PROCEDURE IF EXISTS payroll.runAttendance;
 
-DELIMITER $$ 
-$$
-CREATE PROCEDURE payroll.runAttendance(in pDateFrom varchar(10), in pDateTo varchar(10)) 
+
+DELIMITER $$ $$
+CREATE PROCEDURE payroll.runAttendance (in pDateFrom varchar(10), in pDateTo varchar(10))
 BEGIN
 UPDATE attendance a
-  JOIN vAttendance v ON v.comCode = a.comCode AND v.empCode = a.empCode
-  AND v.dateAt = a.dateAt
-SET a.status = v.status,
-  a.lunch_out = v.lunch_out,
-  a.lunch_in = v.lunch_in,
-  a.evening = v.evening,
-  a.lateMin1 = v.lateMin1,
-  a.lunchMin = v.lunchMin,
-  a.lateMin2 = v.lateMin2,
-  a.workMin = v.workMin,
-  a.otMin = v.otMin
-WHERE a.dateAt BETWEEN pDateFrom AND pDateTo;
-END $$
-DELIMITER ;
+JOIN vAttendance v ON v.comCode = a.comCode
+AND v.empCode = a.empCode
+AND v.dateAt = a.dateAt
+SET
+    a.status = v.status,
+    a.lunch_out = v.lunch_out,
+    a.lunch_in = v.lunch_in,
+    a.evening = v.evening,
+    a.lateMin1 = v.lateMin1,
+    a.lunchMin = v.lunchMin,
+    a.lateMin2 = v.lateMin2,
+    a.workMin = v.workMin,
+    a.otMin = v.otMin
+WHERE
+    a.dateAt BETWEEN pDateFrom AND pDateTo;
+
+
+END $$ DELIMITER;
