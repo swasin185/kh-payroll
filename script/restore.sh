e07f8a27a6e9a1e52c8801f7be2fa7e41b62221f
tom <swasin185@gmail.com>
2026-05-30 13:53:57 +0700

    restore fix

--- script/restore.sh
+++ script/restore.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # set -e
 # shopt -s expand_aliases
-# alias mysql="mysql -uadmin -pTom1973# -h 10.0.0.245"
+# alias mysql="mysql -uadmin -pTom1973# -h 10.0.0.245 payroll"
 
 dbdir=/backup/db/$1
 gunzip $dbdir/payroll.sql.gz
 mysql < $dbdir/payroll.sql
-# sed -i 's/utf8mb4_uca1400_ai_ci/utf8mb4_unicode_ci/g' your_dump_file.sql
+# sed -i 's/utf8mb4_uca1400_ai_ci/utf8mb4_unicode_ci/g' $dbdir/payroll.sql
 gzip $dbdir/payroll.sql
 
 # unalias mysql
