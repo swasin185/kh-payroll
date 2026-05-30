53b6d796a95bbc9a9bcc0568d8d1d525bee9cabc
tom <swasin185@gmail.com>
2026-05-30 13:00:52 +0700

    mysqldump alias

--- script/restore.sh
+++ script/restore.sh
@@ -1,4 +1,12 @@
+set -e
+
+# Enable alias expansion in non-interactive shell and define database alias
+shopt -s expand_aliases
+alias mysql="mysql -uadmin -pTom1973# -h 10.0.0.245"
+
 dbdir=/backup/db/$1
 gunzip $dbdir/payroll.sql.gz
 mysql < $dbdir/payroll.sql
 gzip $dbdir/payroll.sql
+
+unalias mysql
