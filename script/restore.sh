e768a1923cd6393d9b45cf3557678081b212fc0f
tom <swasin185@gmail.com>
2026-05-30 15:09:16 +0700

    sed restore

--- script/restore.sh
+++ script/restore.sh
@@ -5,8 +5,11 @@
 
 dbdir=/backup/db/$1
 gunzip $dbdir/payroll.sql.gz
-mysql < $dbdir/payroll.sql
+# import/export between MariaDB/MySQL
 # sed -i 's/utf8mb4_uca1400_ai_ci/utf8mb4_unicode_ci/g' $dbdir/payroll.sql
+# sed -i 's/NO_AUTO_CREATE_USER,//g' $dbdir/payroll.sql
+# sed -i 's/DEFINER=`[^` ]*`@`[^` ]*`//g' $dbdir/payroll.sql
+mysql < $dbdir/payroll.sql
 gzip $dbdir/payroll.sql
 
 # unalias mysql
